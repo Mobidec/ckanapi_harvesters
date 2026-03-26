94ee168377dbeee8d25ff4696f57934d246a34fd
ifpen-gp <63413841+ifpen-gp@users.noreply.github.com>
2026-03-26 17:41:21 +0100

    tests: added random import test to have at least one test executed for the integration test

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -4,5 +4,9 @@
 This is an integration test example
 Integration tests verify the interaction between multiple components or external systems (e.g., databases, APIs).
 """
+import pytest
 
+from ckanapi_harvesters import BuilderPackage
 
+def test_import():
+    pass
