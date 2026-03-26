7830dee8d34b96f2af350996a09dcb17d0d4998e
ifpen-gp <63413841+ifpen-gp@users.noreply.github.com>
2026-03-26 17:35:23 +0100

    Added empty file to define directory tests/integration/

---
+++ tests/integration/test_integration.py
@@ -0,0 +1,8 @@
+#!python3
+# -*- coding: utf-8 -*-
+"""
+This is an integration test example
+Integration tests verify the interaction between multiple components or external systems (e.g., databases, APIs).
+"""
+
+
