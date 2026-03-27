fe581a81f93d762ec581447e36a4ee0597499012
ifpen-gp <63413841+ifpen-gp@users.noreply.github.com>
2026-03-27 11:23:37 +0100

    Added pytest-cov to requirements and moved test_ckan_demo to tests/ignored

---
+++ tests/local/test_local.py
@@ -0,0 +1,12 @@
+#!python3
+# -*- coding: utf-8 -*-
+"""
+This is an integration test example
+Integration tests verify the interaction between multiple components or external systems (e.g., databases, APIs).
+"""
+import pytest
+
+from ckanapi_harvesters import BuilderPackage
+
+def test_import():
+    pass
